30aa819174690df47cf7e5e9892327e5e258f4e3
sergiomedinaio <63879459+sergiomedinaio@users.noreply.github.com>
2024-06-03 22:25:59 -0300

    feat: agregar lista desde api

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
 const URL = "https://ca1cee33-bafc-422e-b286-7bf52f206284-00-2c9r22u7xggi0.riker.replit.dev"
 
+const showList = (list) => {
+    const container = document.querySelector('.container');
+    list.forEach(element => {
+        container.innerHTML += `
+            <div class="card horizontal">
+                <div class="card-image">
+                <img src="${element.image}">
+                </div>
+                <div class="card-stacked">
+                <div class="card-content">
+                    <p>
+                    ${element.description}
+                    </p>
+                </div>
+                <div class="card-action">
+                    <a href="/detalles/?id=${element.id}">Conocer más</a>
+                </div>
+                </div>
+            </div>
+        `;
+    });
+}
+
 const getList = async () => {
-    const list = await fetch(`${URL}/list/`).then(data => data.json());
-    console.log(list)
+    const result = await fetch(`${URL}/list/`).then(data => data.json());
+    showList(result.plants);
 }
 
 getList();
